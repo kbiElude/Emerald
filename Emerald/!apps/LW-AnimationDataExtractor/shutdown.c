094ad6ba67f0c4fef3eac836482511bbd811e6f1
Dominik Witczak <kbi@theelude.eu>
2014-10-15 22:49:27 +0200

    Adding LW plug-in template

---
+++ Emerald/!apps/LW-AnimationDataExtractor/shutdown.c
@@ -0,0 +1,15 @@
+/*
+ * LWSDK Library Source File
+ * Copyright 1995,1997  NewTek, Inc.
+ *
+ * Default 'Shutdown' implementaion does nothing.
+ */
+#include <lwserver.h>
+
+void Shutdown( void *serverData); // prototype
+
+    void
+Shutdown (
+    void                    *serverData)
+{
+}
