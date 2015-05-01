f71a931565575607d6914a020b4d883da9d7f74a
Dominik Witczak <kbi@theelude.eu>
2015-05-01 22:21:52 +0200

    TestObjectBrowser: Project builds again.

--- Emerald/!apps/Test-ObjectBrowser/src/main.cc
+++ Emerald/!apps/Test-ObjectBrowser/src/main.cc
@@ -1,6 +1,6 @@
 /**
  *
- * Object browser test app (kbi/elude @2012)
+ * Object browser test app (kbi/elude @2012-2015)
  *
  */
 #include "shared.h"
@@ -19,13 +19,16 @@ ogl_context  _context             = NULL;
 system_event _window_closed_event = system_event_create(true, false);
 
 /** Rendering handler */
-void _rendering_handler(ogl_context context, uint32_t n_frames_rendered, system_timeline_time frame_time, void* renderer)
+void _rendering_handler(ogl_context          context,
+                        uint32_t             n_frames_rendered,
+                        system_timeline_time frame_time,
+                        void*                renderer)
 {
     const  ogl_context_gl_entrypoints* entry_points = NULL;
     static int                         counter      = 0;
 
     ogl_context_get_property(context,
-                             OGL_CONTEXT_PROPERTY_ENTRYPOINTS,
+                             OGL_CONTEXT_PROPERTY_ENTRYPOINTS_GL,
                             &entry_points);
 
     if ( (++counter) % 256 == 0)
@@ -33,26 +36,39 @@ void _rendering_handler(ogl_context context, uint32_t n_frames_rendered, system_
         counter = 0;
     }
 
-    entry_points->pGLClearColor(float(counter) / 255.0f, float(counter) / 255.0f, float(counter) / 255.0f, 1.0f);
+    entry_points->pGLClearColor(float(counter) / 255.0f,
+                                float(counter) / 255.0f,
+                                float(counter) / 255.0f,
+                                1.0f);
     entry_points->pGLClear     (GL_COLOR_BUFFER_BIT);
 }
 
 /** Event callback handlers */
-bool _rendering_key_callback_handler(system_window window, unsigned short character, void*)
+bool _rendering_key_callback_handler(system_window  window,
+                                     unsigned short character,
+                                     void*)
 {
     system_event_set(_window_closed_event);
 
     return true;
 }
 
-bool _rendering_lbm_callback_handler(system_window window, unsigned short x, unsigned short y, system_window_vk_status new_status, void*)
+bool _rendering_lbm_callback_handler(system_window           window,
+                                     unsigned short          x,
+                                     unsigned short          y,
+                                     system_window_vk_status new_status,
+                                     void*)
 {
     system_event_set(_window_closed_event);
 
     return true;
 }
 
-bool _rendering_rbm_callback_handler(system_window window, unsigned short x, unsigned short y, system_window_vk_status new_status, void*)
+bool _rendering_rbm_callback_handler(system_window           window,
+                                     unsigned short          x,
+                                     unsigned short          y,
+                                     system_window_vk_status new_status,
+                                     void*)
 {
     curve_editor_show(_context);
 
@@ -61,34 +77,75 @@ bool _rendering_rbm_callback_handler(system_window window, unsigned short x, uns
 
 
 /** Entry point */
-int WINAPI WinMain(HINSTANCE instance_handle, HINSTANCE, LPTSTR, int)
+int WINAPI WinMain(HINSTANCE instance_handle,
+                   HINSTANCE,
+                   LPTSTR,
+                   int)
 {
     int window_size    [2] = {640, 480};
     int window_x1y1x2y2[4] = {0};
-    
+
     /* Add some test curves */
-    curve_container curve_float  = curve_container_create(system_hashed_ansi_string_create("Test1\\zxcv 2d float"),         SYSTEM_VARIANT_FLOAT,   2);
-    curve_container curve_float3 = curve_container_create(system_hashed_ansi_string_create("Test1\\Another test 2d float"), SYSTEM_VARIANT_FLOAT,   2);
-    curve_container curve_float4 = curve_container_create(system_hashed_ansi_string_create("Test1\\aoe 2d float"),          SYSTEM_VARIANT_FLOAT,   2);
-    curve_container curve_float5 = curve_container_create(system_hashed_ansi_string_create("Test1\\2d float"),              SYSTEM_VARIANT_FLOAT,   2);
-    curve_container curve_float2 = curve_container_create(system_hashed_ansi_string_create("Test2\\3d float"),              SYSTEM_VARIANT_FLOAT,   3);
-    curve_container curve_int    = curve_container_create(system_hashed_ansi_string_create("1d int"),                       SYSTEM_VARIANT_INTEGER, 1);
+    curve_container curve_float  = curve_container_create(system_hashed_ansi_string_create("Test1\\zxcv 2d float"),
+                                                          NULL, /* object_manager_path */
+                                                          SYSTEM_VARIANT_FLOAT);
+    curve_container curve_float3 = curve_container_create(system_hashed_ansi_string_create("Test1\\Another test 2d float"),
+                                                          NULL, /* object_manager_path */
+                                                          SYSTEM_VARIANT_FLOAT);
+    curve_container curve_float4 = curve_container_create(system_hashed_ansi_string_create("Test1\\aoe 2d float"),
+                                                          NULL, /* object_manager_path */
+                                                          SYSTEM_VARIANT_FLOAT);
+    curve_container curve_float5 = curve_container_create(system_hashed_ansi_string_create("Test1\\2d float"),
+                                                          NULL, /* object_manager_path */
+                                                          SYSTEM_VARIANT_FLOAT);
+    curve_container curve_float2 = curve_container_create(system_hashed_ansi_string_create("Test2\\3d float"),
+                                                          NULL, /* object_manager_path */
+                                                          SYSTEM_VARIANT_FLOAT);
+    curve_container curve_int    = curve_container_create(system_hashed_ansi_string_create("1d int"),
+                                                          NULL, /* object_manager_path */
+                                                          SYSTEM_VARIANT_INTEGER);
 
     /* Carry on */
-    system_window_get_centered_window_position_for_primary_monitor(window_size, window_x1y1x2y2);
-    
-    system_window         window                   = system_window_create_not_fullscreen(window_x1y1x2y2, system_hashed_ansi_string_create("Test window"), false, 0, false, false, true);
-    ogl_rendering_handler window_rendering_handler = ogl_rendering_handler_create_with_fps_policy(system_hashed_ansi_string_create("Default rendering handler"), 10, _rendering_handler, NULL);
+    system_window_get_centered_window_position_for_primary_monitor(window_size,
+                                                                   window_x1y1x2y2);
 
-    bool context_result = system_window_get_context(window, &_context);
-    ASSERT_DEBUG_SYNC(context_result, "Could not retrieve OGL context");
+    system_window         window                   = system_window_create_not_fullscreen         (OGL_CONTEXT_TYPE_GL,
+                                                                                                  window_x1y1x2y2,
+                                                                                                  system_hashed_ansi_string_create("Test window"),
+                                                                                                  false,
+                                                                                                  0,
+                                                                                                  false,
+                                                                                                  false,
+                                                                                                  true);
+    ogl_rendering_handler window_rendering_handler = ogl_rendering_handler_create_with_fps_policy(system_hashed_ansi_string_create("Default rendering handler"),
+                                                                                                  10,
+                                                                                                  _rendering_handler,
+                                                                                                  NULL);
 
-    system_window_set_rendering_handler (window, window_rendering_handler);
-    system_window_add_callback_func     (window, SYSTEM_WINDOW_CALLBACK_FUNC_PRIORITY_NORMAL, SYSTEM_WINDOW_CALLBACK_FUNC_CHAR,              _rendering_key_callback_handler, NULL);
-    system_window_add_callback_func     (window, SYSTEM_WINDOW_CALLBACK_FUNC_PRIORITY_NORMAL, SYSTEM_WINDOW_CALLBACK_FUNC_LEFT_BUTTON_DOWN,  _rendering_lbm_callback_handler, NULL);
-    system_window_add_callback_func     (window, SYSTEM_WINDOW_CALLBACK_FUNC_PRIORITY_NORMAL, SYSTEM_WINDOW_CALLBACK_FUNC_RIGHT_BUTTON_DOWN, _rendering_rbm_callback_handler, NULL);
+    system_window_get_property(window,
+                               SYSTEM_WINDOW_PROPERTY_RENDERING_CONTEXT,
+                              &_context);
 
-    ogl_rendering_handler_play(window_rendering_handler, 0);
+    system_window_set_rendering_handler(window,
+                                        window_rendering_handler);
+    system_window_add_callback_func    (window,
+                                        SYSTEM_WINDOW_CALLBACK_FUNC_PRIORITY_NORMAL,
+                                        SYSTEM_WINDOW_CALLBACK_FUNC_CHAR,
+                                        _rendering_key_callback_handler,
+                                        NULL);
+    system_window_add_callback_func    (window,
+                                        SYSTEM_WINDOW_CALLBACK_FUNC_PRIORITY_NORMAL,
+                                        SYSTEM_WINDOW_CALLBACK_FUNC_LEFT_BUTTON_DOWN,
+                                        _rendering_lbm_callback_handler,
+                                        NULL);
+    system_window_add_callback_func    (window,
+                                        SYSTEM_WINDOW_CALLBACK_FUNC_PRIORITY_NORMAL,
+                                        SYSTEM_WINDOW_CALLBACK_FUNC_RIGHT_BUTTON_DOWN,
+                                        _rendering_rbm_callback_handler,
+                                        NULL);
+
+    ogl_rendering_handler_play(window_rendering_handler,
+                               0);
 
     system_event_wait_single_infinite(_window_closed_event);
 
