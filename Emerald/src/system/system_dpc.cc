b8f7bf4ddbff46b86ae73cd9bcf2be93261b8b19
Dominik Witczak <kbi@theelude.eu>
2016-11-28 20:32:25 +0100

    Test-SceneViewer: Fully functional now :-)

--- Emerald/src/system/system_dpc.cc
+++ Emerald/src/system/system_dpc.cc
@@ -2,21 +2,21 @@
 #include "system/system_dpc.h"
 #include "system/system_hash64map.h"
 #include "system/system_read_write_mutex.h"
+#include "system/system_resizable_vector.h"
 #include "system/system_resource_pool.h"
 #include "system/system_thread_pool.h"
 
-#define MAX_SCHEDULED_CALLBACKS (8)
 
-PRIVATE system_hash64map        _active_dpcs       = nullptr;
-PRIVATE system_read_write_mutex _active_dpcs_mutex = nullptr;
-PRIVATE system_resource_pool    _dpc_item_pool     = nullptr;
+PRIVATE system_hash64map        _active_dpcs            = nullptr;
+PRIVATE system_read_write_mutex _active_dpcs_mutex      = nullptr;
+PRIVATE system_resource_pool    _dpc_item_callback_pool = nullptr;
+PRIVATE system_resource_pool    _dpc_item_pool          = nullptr;
 
 struct _system_dpc_item;
 
 typedef struct
 {
     void*                    callback_arg;
-    uint32_t                 n_callback_slot;
     void*                    object;
     _system_dpc_item*        owner_ptr;
     PFNSYSTEMDPCCALLBACKPROC pfn_callback_proc;
@@ -24,8 +24,24 @@ typedef struct
 
 typedef struct _system_dpc_item
 {
-    uint32_t                  n_scheduled_callbacks;
-    _system_dpc_item_callback scheduled_callbacks[MAX_SCHEDULED_CALLBACKS];
+    system_resizable_vector scheduled_callbacks_vector;
+
+
+    static void deinit(system_resource_pool_block item_raw_ptr)
+    {
+        _system_dpc_item* item_ptr = reinterpret_cast<_system_dpc_item*>(item_raw_ptr);
+
+        system_resizable_vector_release(item_ptr->scheduled_callbacks_vector);
+        item_ptr->scheduled_callbacks_vector = nullptr;
+    }
+
+    static void init(system_resource_pool_block item_raw_ptr)
+    {
+        _system_dpc_item* item_ptr = reinterpret_cast<_system_dpc_item*>(item_raw_ptr);
+
+        item_ptr->scheduled_callbacks_vector = system_resizable_vector_create(8); /* capacity */
+    }
+
 } _system_dpc_item;
 
 
@@ -43,24 +59,44 @@ PRIVATE volatile void _system_dpc_thread_pool_callback(system_thread_pool_callba
     system_read_write_mutex_lock(_active_dpcs_mutex,
                                  ACCESS_WRITE);
     {
+        uint32_t callback_index        = -1;
+        uint32_t n_scheduled_callbacks = -1;
+
         ASSERT_DEBUG_SYNC(system_hash64map_contains(_active_dpcs,
                                                     object_hash64),
                           "TODO: Add support for multiple active DPCs per object instance");
 
-        if (--callback_data_ptr->owner_ptr->n_scheduled_callbacks == 0)
-        {
-            system_resource_pool_return_to_pool(_dpc_item_pool,
-                                                reinterpret_cast<system_resource_pool_block>(callback_data_ptr->owner_ptr) );
+        system_resizable_vector_get_property(callback_data_ptr->owner_ptr->scheduled_callbacks_vector,
+                                             SYSTEM_RESIZABLE_VECTOR_PROPERTY_N_ELEMENTS,
+                                            &n_scheduled_callbacks);
 
-            system_hash64map_remove(_active_dpcs,
-                                    object_hash64);
+        if (n_scheduled_callbacks > 1)
+        {
+            callback_index = system_resizable_vector_find(callback_data_ptr->owner_ptr->scheduled_callbacks_vector,
+                                                          callback_data_ptr);
+            ASSERT_DEBUG_SYNC(callback_index != ITEM_NOT_FOUND,
+                              "DPC item callback was not found in the DPC item stash?!");
+
+            system_resizable_vector_delete_element_at(callback_data_ptr->owner_ptr->scheduled_callbacks_vector,
+                                                      callback_index);
         }
         else
         {
-            memset(callback_data_ptr->owner_ptr->scheduled_callbacks + callback_data_ptr->n_callback_slot,
-                   0,
-                   sizeof(_system_dpc_item_callback) );
+            system_resizable_vector_clear(callback_data_ptr->owner_ptr->scheduled_callbacks_vector);
+
+            system_hash64map_remove(_active_dpcs,
+                                    object_hash64);
+
+            system_resource_pool_return_to_pool(_dpc_item_pool,
+                                                reinterpret_cast<system_resource_pool_block>(callback_data_ptr->owner_ptr) );
         }
+
+        memset(callback_data_ptr,
+               0,
+               sizeof(*callback_data_ptr) );
+
+        system_resource_pool_return_to_pool(_dpc_item_callback_pool,
+                                            reinterpret_cast<system_resource_pool_block>(callback_data_ptr) );
     }
     system_read_write_mutex_unlock(_active_dpcs_mutex,
                                    ACCESS_WRITE);
@@ -90,6 +126,9 @@ PUBLIC void system_dpc_deinit()
     system_read_write_mutex_release(_active_dpcs_mutex);
     _active_dpcs_mutex = nullptr;
 
+    system_resource_pool_release(_dpc_item_callback_pool);
+    _dpc_item_callback_pool = nullptr;
+
     system_resource_pool_release(_dpc_item_pool);
     _dpc_item_pool = nullptr;
 }
@@ -97,12 +136,16 @@ PUBLIC void system_dpc_deinit()
 /** Please see header for specification */
 PUBLIC void system_dpc_init()
 {
-    _active_dpcs       = system_hash64map_create       (sizeof(_system_dpc_item*) ); /* should_be_thread_safe */
-    _active_dpcs_mutex = system_read_write_mutex_create();
-    _dpc_item_pool     = system_resource_pool_create   (sizeof(_system_dpc_item),
-                                                        64,       /* n_elements_to_preallocate */
-                                                        nullptr,  /* init_fn                   */
-                                                        nullptr); /* deinit_fn                 */
+    _active_dpcs            = system_hash64map_create       (sizeof(_system_dpc_item*) );
+    _active_dpcs_mutex      = system_read_write_mutex_create();
+    _dpc_item_callback_pool = system_resource_pool_create   (sizeof(_system_dpc_item_callback),
+                                                             64,       /* n_elements_to_preallocate */
+                                                             nullptr,  /* init_fn                   */
+                                                             nullptr); /* deinit_fn                 */
+    _dpc_item_pool          = system_resource_pool_create   (sizeof(_system_dpc_item),
+                                                             64,       /* n_elements_to_preallocate */
+                                                             _system_dpc_item::init,
+                                                             _system_dpc_item::deinit);
 }
 
 /** Please see header for specification */
@@ -133,70 +176,35 @@ PUBLIC void system_dpc_schedule(void*                    object,
     system_read_write_mutex_lock(_active_dpcs_mutex,
                                  ACCESS_WRITE);
     {
-        _system_dpc_item* dpc_item_ptr = nullptr;
+        _system_dpc_item*          dpc_item_ptr          = nullptr;
+        _system_dpc_item_callback* dpc_item_callback_ptr = nullptr;
 
         if (!system_hash64map_get(_active_dpcs,
                                   object_hash64,
                                  &dpc_item_ptr) )
         {
-            _system_dpc_item* new_item_ptr = reinterpret_cast<_system_dpc_item*>(system_resource_pool_get_from_pool(_dpc_item_pool) );
-
-            memset(new_item_ptr,
-                   0,
-                   sizeof(_system_dpc_item) );
-
-            new_item_ptr->scheduled_callbacks[0].callback_arg      = callback_arg;
-            new_item_ptr->scheduled_callbacks[0].n_callback_slot   = 0;
-            new_item_ptr->scheduled_callbacks[0].object            = object;
-            new_item_ptr->scheduled_callbacks[0].owner_ptr         = new_item_ptr;
-            new_item_ptr->scheduled_callbacks[0].pfn_callback_proc = pfn_callback_proc;
-            new_item_ptr->n_scheduled_callbacks                    = 1;
+            dpc_item_ptr = reinterpret_cast<_system_dpc_item*>(system_resource_pool_get_from_pool(_dpc_item_pool) );
 
             system_hash64map_insert(_active_dpcs,
                                     object_hash64,
-                                    new_item_ptr,
+                                    dpc_item_ptr,
                                     nullptr,  /* on_removal_callback          */
                                     nullptr); /* on_removal_callback_user_arg */
-
-            dpc_task = system_thread_pool_create_task_handler_only(THREAD_POOL_TASK_PRIORITY_NORMAL,
-                                                                   _system_dpc_thread_pool_callback,
-                                                                   new_item_ptr->scheduled_callbacks + 0);
         }
-        else
-        {
-            ASSERT_DEBUG_SYNC(dpc_item_ptr->n_scheduled_callbacks < MAX_SCHEDULED_CALLBACKS,
-                              "TODO");
 
-            /* Identify a free slot */
-            static const _system_dpc_item_callback dummy_callback = {0};
-            uint32_t                               n_callback_slot;
+        dpc_item_callback_ptr = reinterpret_cast<_system_dpc_item_callback*>(system_resource_pool_get_from_pool(_dpc_item_callback_pool) );
 
-            for (n_callback_slot = 0;
-                 n_callback_slot < MAX_SCHEDULED_CALLBACKS;
-               ++n_callback_slot)
-            {
-                if (memcmp(dpc_item_ptr->scheduled_callbacks + n_callback_slot,
-                          &dummy_callback,
-                           sizeof(dummy_callback)) == 0)
-                {
-                    break;
-                }
-            }
+        dpc_item_callback_ptr->callback_arg      = callback_arg;
+        dpc_item_callback_ptr->object            = object;
+        dpc_item_callback_ptr->owner_ptr         = dpc_item_ptr;
+        dpc_item_callback_ptr->pfn_callback_proc = pfn_callback_proc;
 
-            ASSERT_DEBUG_SYNC(n_callback_slot != MAX_SCHEDULED_CALLBACKS,
-                              "TODO: Add support for this scenario");
+        system_resizable_vector_push(dpc_item_ptr->scheduled_callbacks_vector,
+                                     dpc_item_callback_ptr);
 
-            dpc_item_ptr->scheduled_callbacks[n_callback_slot].callback_arg      = callback_arg;
-            dpc_item_ptr->scheduled_callbacks[n_callback_slot].n_callback_slot   = n_callback_slot;
-            dpc_item_ptr->scheduled_callbacks[n_callback_slot].object            = object;
-            dpc_item_ptr->scheduled_callbacks[n_callback_slot].owner_ptr         = dpc_item_ptr;
-            dpc_item_ptr->scheduled_callbacks[n_callback_slot].pfn_callback_proc = pfn_callback_proc;
-            dpc_item_ptr->n_scheduled_callbacks++;
-
-            dpc_task = system_thread_pool_create_task_handler_only(THREAD_POOL_TASK_PRIORITY_NORMAL,
-                                                                   _system_dpc_thread_pool_callback,
-                                                                   dpc_item_ptr->scheduled_callbacks + n_callback_slot);
-        }
+        dpc_task = system_thread_pool_create_task_handler_only(THREAD_POOL_TASK_PRIORITY_NORMAL,
+                                                               _system_dpc_thread_pool_callback,
+                                                               dpc_item_callback_ptr);
     }
     system_read_write_mutex_unlock(_active_dpcs_mutex,
                                    ACCESS_WRITE);
